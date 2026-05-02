c406588be89ac33c27fa38ec4e827f83f70f7d4a
Chris <chris@jj-reselling.store>
2026-05-02 03:28:20 +0200

    fix: msvc libs & printf conflict

--- crt/io/printf.h
+++ crt/io/printf.h
@@ -8,3 +8,9 @@ struct PrintBuf
 };
 
 extern "C" int printf(const char* pFmt, ...);
+
+namespace std
+{
+    // std:c++latest pulls in MSVC headers this is why we need to put this in our own header to avoid conflicts
+    using ::printf;
+}
