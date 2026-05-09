e1145414f12c9c3f93383e00443e25f93c78045d
Chris <chris@jj-reselling.store>
2026-05-09 14:03:22 +0200

    feat: add memset & memcpy as extern "C"

--- memory/heap/heap.cpp
+++ memory/heap/heap.cpp
@@ -1,5 +1,6 @@
 #include "heap.h"
 #include "../pool/pool.h"
+#include <cstdint>
 
 bool Heap::Init()
 {
@@ -54,4 +55,30 @@ extern "C" void* malloc(size_t iSize)
 extern "C" void free(void* pPtr)
 {
     Pool::Free(pPtr);
+}
+
+extern "C" void* memset(void* pDst, int iVal, size_t iLen)
+{
+    uint8_t* pBytes = (uint8_t*)pDst;
+    uint8_t bVal = (uint8_t)iVal;
+
+    for (size_t i = 0; i < iLen; i++)
+    {
+        pBytes[i] = bVal;
+    }
+
+    return pDst;
+}
+
+extern "C" void* memcpy(void* pDst, const void* pSrc, size_t iLen)
+{
+    uint8_t* pD = (uint8_t*)pDst;
+    const uint8_t* pS = (const uint8_t*)pSrc;
+
+    for (size_t i = 0; i < iLen; i++)
+    {
+        pD[i] = pS[i];
+    }
+
+    return pDst;
 }
